355e519bb85ec1b74b5084f7e1555c023edf2d0d
CrownRu <100330653+CrownRu@users.noreply.github.com>
2023-03-08 16:04:37 +0800

    Delete Mail directory

--- Mail/DBconn.php
+++
@@ -1,7 +0,0 @@
-<?php
-$link = @mysqli_connect( 
-    'localhost',
-    'root',
-    '',
-    'vote');
-?>
--- Mail/mailform.php
+++
@@ -1,8 +0,0 @@
-<form action='mailsend.php' method='post'>
-
-Email:<input type="email" name="email"></br>
-Name:<input type="text" name="uName"></br>
-留言:<textarea name="message" rows="10" cols="20"></textarea></br>
-<input type="submit">
-
-</form>
